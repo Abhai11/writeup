85a2a451221a716e2df80aa2bd1df1935d8f7c5f
Abhai <abhairaj.singh@gmail.com>
2020-08-22 21:30:26 +0100

    Initializing the repo and added producer consumer implementation using blocking queue.

---
+++ src/main/java/com/learning/concurrency/ProducerConsumerBQ.java
@@ -0,0 +1,142 @@
+package com.learning.concurrency;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Producer-Consumer Problem: A multi-process problem where we have a producer process which is producing some data into a buffer and
+ * a consumer process which is consuming the data produced from the buffer.
+ * Below are some constraints:
+ * 1. Producer adds the data into a buffer and it should wait if the buffer is full.
+ * 2. In case the buffer is empty consumer should wait before fetching new data.
+ * 3. Consumer thread should shutdown once it receives a shutdown signal.
+ * <p>
+ * Solution:
+ * One of the easiest way to solve this problem is to use Blocking Queue to synchronize the producer and consumer processes.
+ * Blocking Queue is a special type of queue implementation which supports below operations:
+ * 1. Blocks producer thread if queue is full until some space is available.
+ * 2. Blocks Consumer thread if queue is empty until some new data is added.
+ *
+ * In order to shutdown the consumer thread we can produce some kind of special value which acts as the shutdown signal/silver bullet
+ * and consumer thread exits once it received it.
+ */
+
+public class ProducerConsumerBQ {
+
+    public static void main(String[] args) {
+        BlockingQueue<Integer> dataQueue = new ArrayBlockingQueue(10);
+        System.out.println("**** Starting Producer thread ******");
+        /*new Thread(new Producer(dataQueue)).start();*/
+        new Thread(new SlowProducer(dataQueue)).start();
+        new Thread(new Consumer(dataQueue)).start();
+        /*new Thread(new SlowConsumer(dataQueue)).start();*/
+    }
+}
+
+class Producer implements Runnable {
+    BlockingQueue<Integer> queue;
+
+    public Producer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                System.out.println("#####" + Thread.currentThread().getName() + "| Producing " + i + " #####");
+                queue.put(i);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+        try {
+            System.out.println("Producer thread shutting down. Sending a shutdown signal to consumer.");
+            queue.put(-1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    BlockingQueue<Integer> queue;
+
+    public Consumer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        boolean shutdown = false;
+        while (!shutdown) {
+            try {
+                Integer i = queue.take();
+                if (i.equals(-1)) {
+                    System.out.println("Shutdown signal received.");
+                    shutdown = true;
+                } else {
+                    System.out.println("**** " + Thread.currentThread().getName() + "| Consuming " + i + " *****");
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+        System.out.println("consumer shutting down");
+    }
+}
+
+class SlowProducer implements Runnable {
+    BlockingQueue<Integer> queue;
+
+    public SlowProducer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                System.out.println("#####" + Thread.currentThread().getName() + "| Producing " + i + " #####");
+                Thread.sleep(500);
+                queue.put(i);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+        try {
+            System.out.println("Producer thread shutting down. Sending a shutdown signal to consumer.");
+            queue.put(-1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class SlowConsumer implements Runnable {
+    BlockingQueue<Integer> queue;
+
+    public SlowConsumer(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        boolean shutdown = false;
+        while (!shutdown) {
+            try {
+                Integer i = queue.take();
+                if (i.equals(-1)) {
+                    System.out.println("Shutdown signal received.");
+                    shutdown = true;
+                } else {
+                    System.out.println("**** " + Thread.currentThread().getName() + "| Consuming " + i + " *****");
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+        }
+        System.out.println("consumer shutting down");
+    }
+}
